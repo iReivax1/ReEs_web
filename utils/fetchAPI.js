9df1c58b9483f0a117d217989cf04dd6e764eb80
xavier <xavi0007@e.ntu.edu.sg>
2022-08-02 16:44:50 +0800

    TODO: reset filters

--- utils/fetchAPI.js
+++ utils/fetchAPI.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
     const {data} = await axios.get((url), {
         headers:{
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': '90442b3432mshd7508bb49bf9a90p1673d7jsn897e9e1b004a',
+            'x-rapidapi-key': process.env.BAYUT_KEY
         }
     });
 
