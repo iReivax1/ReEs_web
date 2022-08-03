8e1bcfd0fc2435fc558367bcb1e14f71e58c7e4d
xavier <xavi0007@e.ntu.edu.sg>
2022-08-03 21:59:15 +0800

    rm image bar

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -5,11 +5,10 @@ import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
 import { baseUrl, fetchApi } from '../../utils/fetchApi';
-import imageScrollbar from '../../components/imageScrollbar';
 
 const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, purpose, furnishingStatus, amenities, photos } }) => (
     <Box maxWidth='1000px' margin='auto' p='4'>
-        {photos && <imageScrollbar data={photos} />}
+        {/* {photos && <imageScrollbar data={photos} />} */}
         <Box w='full' p='6'>
             <Flex paddingTop='2' alignItems='center'>
                 <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
