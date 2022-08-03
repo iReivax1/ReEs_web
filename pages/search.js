ebe4514f3f5e75932361b4f55f9100ed8fa93b80
xavier <xavi0007@e.ntu.edu.sg>
2022-08-03 11:07:48 +0800

    name change

--- pages/search.js
+++ pages/search.js
@@ -6,7 +6,7 @@ import { BsFilter } from "react-icons/bs";
 import SearchFilters from "../components/SearchFilters";
 import Property from "../components/Property";
 import noresult from '../assets/images/noresult.jpg'
-import { fetchApi, baseUrl } from "../utils/fetchapi";
+import { fetchApi, baseUrl } from "../utils/fetchApi";
 
 const Search = ({properties}) => {
     const [searchFilters, setSearchFilters] = useState(false);
